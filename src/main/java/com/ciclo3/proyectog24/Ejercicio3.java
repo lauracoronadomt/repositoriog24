4124de0316ac33066876bc6a1acb76d2bec1d46b
Laura Coronado <laura.coronado.mt@usa.edu.co>
2022-09-14 18:40:43 -0500

    agregando Clase Ejercicio3.java

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author Laura
+ */
+public class Ejercicio3 {
+   public void metodo() {
+        System.out.println("Hola sigo programando"); 
+     
+} 
+}
