f99636929712fe8698b67012d82942202bd522ba
Laura Coronado <laura.coronado.mt@usa.edu.co>
2022-09-13 19:24:04 -0500

    mi primer commit

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio1.java
@@ -0,0 +1,15 @@
+
+package com.ciclo3.proyectog24;
+
+import static java.time.Clock.system;
+
+/**
+ *
+ * @author Laura
+ */
+public class Ejercicio1 {
+    public void metodo() {
+        System.out.println("Logica metodo"); 
+    }
+    
+}
